dc838cf353ec6386cee7d5bcda17f0152b9e79dc
Akihisa Yagi <capra314cabra@gmail.com>
2021-05-31 21:03:53 +0900

    Use SQL server

---
+++ server/initialize-command.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Problems (
+    SourceID int,
+    ProblemText text,
+    AnswerText text,
+    Correct int,
+    Wrong int
+);
+
+CREATE TABLE Sources (
+    SourceID int,
+    Title text
+);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import quizSets from "./quiz-sets";
 
 const app = express();
 
@@ -8,7 +9,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const router = express.Router();
-router.get('/', (_, res) => res.send("Hello World!"));
-app.use(router);
 
+router.get('/', async (req, res) => {
+    if (req.query.id) {
+        let id = parseInt(req.query.id as string);
+        let quiz = await quizSets.get_quiz(id);
+        let result = quiz
+            .map((val) => `${val.ProblemText}, ${val.AnswerText}`)
+            .join("\n");
+        res.send(result);
+    }
+    else {
+        res.send("Please give me an id.")
+    }
+});
+
+app.use(router);
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
